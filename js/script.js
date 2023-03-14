2c2c3d2405752d4179f9b04e6fd1c82053f8ca84
Leoodaviid <david_ax7@hotmail.com>
2023-03-14 13:13:51 -0300

    finalizando desenhos do campo + placar

--- js/script.js
+++ js/script.js
@@ -12,17 +12,32 @@ function setup() {
 
 
 function draw() {
+    // desenho do campo
     canvasCtx.fillStyle = "#286047"
     canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
 
+    // desenho da linha central
     canvasCtx.fillStyle = "#ffffff"
+    canvasCtx.fillRect(window.innerWidth / 2 - lineWidth / 2, 0, lineWidth, window.innerHeight)
 
-    const x = window.innerWidth / 2 - lineWidth / 2
-    const y = 0
-    const w = lineWidth
-    const h = window.innerHeight
+    // desenho da raquete esquerda
+    canvasCtx.fillRect(10, 100, lineWidth, 200)
 
-    canvasCtx.fillRect(x, y, w, h)
+    // desenho da raquete direita
+    canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 120, lineWidth, 200)
+
+    // desenho da bola
+    canvasCtx.beginPath()
+    canvasCtx.arc(500, 300, 20, 0, 2 * Math.PI, false)
+    canvasCtx.fill()
+
+    // desenho do placar
+    canvasCtx.font = "bold 72px Arial"
+    canvasCtx.textAlign = "center"
+    canvasCtx.textBaseline = "top"
+    canvasCtx.fillStyle = "#01341d"
+    canvasCtx.fillText('3', window.innerWidth / 4, 50)
+    canvasCtx.fillText('1', window.innerWidth / 4 + window.innerWidth / 2, 50)
 }
 
 
