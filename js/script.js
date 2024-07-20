fd43c0b50853756f0cdda5245e68d6c9f0cb3866
Leoodaviid <david_ax7@hotmail.com>
2024-07-20 14:48:05 -0300

    feat: add game start and reset functionality with countdown and improved paddle boundary detection

--- js/script.js
+++ js/script.js
@@ -1,203 +1,359 @@
 const canvasEl = document.querySelector("canvas"),
-    canvasCtx = canvasEl.getContext("2d"),
-    gapX = 10
+  canvasCtx = canvasEl.getContext("2d"),
+  gapX = 10;
+
+let gameStarted = false;
+let countdown = 3;
+let winner = null;
 
 const mouse = {
-    x: 0,
-    y: 0
-}
+  x: 0,
+  y: 0,
+};
+
+const margin = 90;
 
 const field = {
-    w: window.innerWidth,
-    h: window.innerHeight,
-    draw: function () {
-        canvasCtx.fillStyle = "#286047"
-        canvasCtx.fillRect(0, 0, this.w, this.h)
-    },
-}
+  w: window.innerWidth - 2 * margin,
+  h: window.innerHeight - 2 * margin,
+  draw: function () {
+    // Desenhar a margem
+    canvasCtx.fillStyle = "#01341d";
+    canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+    // Desenhar o campo
+    canvasCtx.fillStyle = "#286047";
+    canvasCtx.fillRect(margin, margin, this.w, this.h);
+  },
+};
 
 const line = {
-    w: 15,
-    h: field.h,
-    draw: function () {
-        canvasCtx.fillStyle = "#ffffff"
-        canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, this.h)
-    },
-}
+  w: 15,
+  h: window.innerHeight - 2 * margin,
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(
+      field.w / 2 + margin - this.w / 2,
+      margin,
+      this.w,
+      this.h
+    );
+  },
+};
 
 const leftPaddle = {
-    x: gapX,
-    y: 0,
-    w: line.w,
-    h: 200,
-    _move: function () {
-        this.y = mouse.y - this.h / 2
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#ffffff"
-        canvasCtx.fillRect(this.x, this.y, this.w, this.h)
+  x: margin + gapX,
+  y: margin,
+  w: 15,
+  h: 200,
+  _move: function () {
+    this.y = mouse.y - this.h / 2;
+    if (this.y < margin) this.y = margin;
+    if (this.y + this.h > field.h + margin) this.y = field.h + margin - this.h;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
 
-        this._move()
-    },
-}
+    this._move();
+  },
+};
 
 const rightPaddle = {
-    x: field.w - line.w - gapX,
-    y: 0,
-    w: line.w,
-    h: 200,
-    speed: 5,
-    _move: function () {
-        const shouldMiss = Math.random() < 0.1; // 10% de chance de errar
-        if (shouldMiss || this.y + this.h / 2 < ball.y + ball.r) {
-            this.y += this.speed;
-        } else {
-            this.y -= this.speed;
-        }
-    },
-    _speedUp: function () {
-        this.speed += 2
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#ffffff"
-        canvasCtx.fillRect(this.x, this.y, this.w, this.h)
+  x: window.innerWidth - margin - gapX - 15,
+  y: margin,
+  w: 15,
+  h: 200,
+  speed: 5,
+  _move: function () {
+    if (this.y + this.h / 2 < ball.y) {
+      this.y += this.speed;
+    } else {
+      this.y -= this.speed;
+    }
+    if (this.y < margin) this.y = margin;
+    if (this.y + this.h > field.h + margin) this.y = field.h + margin - this.h;
+  },
+  _speedUp: function () {
+    this.speed += 2;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
 
-        this._move()
-    },
-}
+    this._move();
+  },
+};
 
 const score = {
-    human: 0,
-    computer: 0,
-    increaseHuman: function () {
-        this.human++
-    },
-    increaseComputer: function () {
-        this.computer++
-    },
-    draw: function () {
-        canvasCtx.font = "bold 72px Arial"
-        canvasCtx.textAlign = "center"
-        canvasCtx.textBaseline = "top"
-        canvasCtx.fillStyle = "#01341d"
-        canvasCtx.fillText(this.human, field.w / 4, 50)
-        canvasCtx.fillText(this.computer, field.w / 4 + field.w / 2, 50)
-    }
-}
+  human: 0,
+  computer: 0,
+  increaseHuman: function () {
+    this.human++;
+  },
+  increaseComputer: function () {
+    this.computer++;
+  },
+  draw: function () {
+    canvasCtx.font = "bold 72px Arial";
+    canvasCtx.textAlign = "center";
+    canvasCtx.textBaseline = "top";
+    canvasCtx.fillStyle = "#01341d";
+    canvasCtx.fillText(this.human, field.w / 4 + margin, margin + 50);
+    canvasCtx.fillText(this.computer, (3 * field.w) / 4 + margin, margin + 50);
+  },
+  reset: function () {
+    this.human = 0;
+    this.computer = 0;
+  },
+};
 
 const ball = {
-    x: field.w / 2,
-    y: field.h / 2,
-    r: 20,
-    speed: 4,
-    directionX: 1,
-    directionY: 1,
-    _calcPosition: function () {
-        // verifica se o jogador 1 fez um ponto (x > largura do campo)
-        if (this.x > field.w - this.r - rightPaddle.w - gapX) {
-            // verifica se a raquete direita está na posição da bola
-            if (this.y + this.r > rightPaddle.y && this.y - this.r < rightPaddle.y + rightPaddle.h) {
-                // rebate a bola invertendo o sinal de x
-                this._reverseX()
-            } else {
-                // pontuar o jogador 1
-                score.increaseHuman()
-                this._pointUp()
-            }
-        }
-        // verifica se o jogador 2 fez um ponto(x < 0)
-        if (this.x < this.r + leftPaddle.w + gapX) {
-            // verifica se a raquete esquerda esta na posição y da bola
-            if (this.y + this.r > leftPaddle.y && this.y - this.r < leftPaddle.y + leftPaddle.h) {
-                // rebate a bola invertendo o sinal de x
-                this._reverseX()
-            } else {
-                // pontuar o jogador 2
-                score.increaseComputer()
-                this._pointUp()
-            }
-        }
-
-        // verifica as laterais superior e inferior do campo
-        if (
-            (this.y - this.r < 0 && this.directionY < 0) ||
-            (this.y > field.h - this.r && this.directionY > 0)) {
-
-            // rebate a bola invertendo o sinal do eixo y    
-            this._reverseY()
-        }
-    },
-    _reverseX: function () {
-        // 1 * -1 = -1 
-        // -1 * -1 = 1
-        this.directionX *= -1
-    },
-    _reverseY: function () {
-        this.directionY *= -1
-    },
-    _speedUp: function () {
-        this.speed += 2
-    },
-    _pointUp: function () {
-        this._speedUp()
-        rightPaddle._speedUp()
-        this.x = field.w / 2
-        this.y = field.h / 2
-    },
-    _move: function () {
-        this.x += this.directionX * this.speed
-        this.y += this.directionY * this.speed
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#ffffff"
-        canvasCtx.beginPath()
-        canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
-        canvasCtx.fill()
-
-        this._calcPosition()
-        this._move()
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2,
+  r: 20,
+  speed: 4,
+  directionX: 1,
+  directionY: 1,
+  _calcPosition: function () {
+    if (this.x > field.w - this.r + margin - rightPaddle.w - gapX) {
+      if (
+        this.y + this.r > rightPaddle.y &&
+        this.y - this.r < rightPaddle.y + rightPaddle.h
+      ) {
+        this._reverseX();
+      } else {
+        score.increaseHuman();
+        this._pointUp();
+      }
     }
-}
+    if (this.x < this.r + leftPaddle.w + gapX + margin) {
+      if (
+        this.y + this.r > leftPaddle.y &&
+        this.y - this.r < leftPaddle.y + leftPaddle.h
+      ) {
+        this._reverseX();
+      } else {
+        score.increaseComputer();
+        this._pointUp();
+      }
+    }
+    if (
+      (this.y - this.r < margin && this.directionY < 0) ||
+      (this.y + this.r > field.h + margin && this.directionY > 0)
+    ) {
+      this._reverseY();
+    }
+  },
+  _reverseX: function () {
+    this.directionX *= -1;
+  },
+  _reverseY: function () {
+    this.directionY *= -1;
+  },
+  _speedUp: function () {
+    this.speed += 2;
+  },
+  _pointUp: function () {
+    this._speedUp();
+    rightPaddle._speedUp();
+    this.x = field.w / 2 + margin;
+    this.y = field.h / 2 + margin;
+    gameStarted = false;
+    countdown = 3;
+    startCountdown();
+  },
+  _move: function () {
+    this.x += this.directionX * this.speed;
+    this.y += this.directionY * this.speed;
+  },
+  draw: function () {
+    // Desenhar a bola
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.beginPath();
+    canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+    canvasCtx.fill();
+
+    // Desenhar detalhes da bola (costuras)
+    canvasCtx.strokeStyle = "#000000";
+    canvasCtx.lineWidth = 2;
+
+    // Desenhar a primeira costura
+    canvasCtx.beginPath();
+    canvasCtx.arc(
+      this.x,
+      this.y,
+      this.r - 3,
+      0.25 * Math.PI,
+      1.25 * Math.PI,
+      false
+    );
+    canvasCtx.stroke();
+
+    // Desenhar a segunda costura
+    canvasCtx.beginPath();
+    canvasCtx.arc(
+      this.x,
+      this.y,
+      this.r - 3,
+      1.75 * Math.PI,
+      0.75 * Math.PI,
+      false
+    );
+    canvasCtx.stroke();
+
+    if (gameStarted) {
+      this._calcPosition();
+      this._move();
+    }
+  },
+};
+
+// Adicionar botões de iniciar e resetar
+const buttons = {
+  startButton: {
+    x: window.innerWidth / 2 - 210,
+    y: window.innerHeight - margin + 20,
+    w: 200,
+    h: 50,
+    draw: function () {
+      canvasCtx.fillStyle = "#286047";
+      canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+      canvasCtx.fillStyle = "#ffffff";
+      canvasCtx.font = "bold 24px Arial";
+      canvasCtx.textAlign = "center";
+      canvasCtx.textBaseline = "middle";
+      canvasCtx.fillText("Iniciar", this.x + this.w / 2, this.y + this.h / 2);
+    },
+    onClick: function () {
+      resetGame();
+      startCountdown();
+    },
+  },
+  resetButton: {
+    x: window.innerWidth / 2 + 10,
+    y: window.innerHeight - margin + 20,
+    w: 200,
+    h: 50,
+    draw: function () {
+      canvasCtx.fillStyle = "#286047";
+      canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+      canvasCtx.fillStyle = "#ffffff";
+      canvasCtx.font = "bold 24px Arial";
+      canvasCtx.textAlign = "center";
+      canvasCtx.textBaseline = "middle";
+      canvasCtx.fillText("Resetar", this.x + this.w / 2, this.y + this.h / 2);
+    },
+    onClick: function () {
+      resetGame();
+    },
+  },
+};
 
 function setup() {
-    canvasEl.width = canvasCtx.width = field.w
-    canvasEl.height = canvasCtx.height = field.h
-
+  canvasEl.width = window.innerWidth;
+  canvasEl.height = window.innerHeight;
 }
-
 
 function draw() {
-    field.draw()
-    line.draw()
-    leftPaddle.draw()
-    rightPaddle.draw()
-    score.draw()
-    ball.draw()
+  field.draw();
+  line.draw();
+  leftPaddle.draw();
+  rightPaddle.draw();
+  score.draw();
+  ball.draw();
+  buttons.startButton.draw();
+  buttons.resetButton.draw();
 }
 
-
+function drawCountdown() {
+  field.draw();
+  line.draw();
+  score.draw();
+  buttons.startButton.draw();
+  buttons.resetButton.draw();
+  canvasCtx.font = "bold 144px Arial";
+  canvasCtx.textAlign = "center";
+  canvasCtx.textBaseline = "middle";
+  canvasCtx.fillStyle = "#01341d";
+  canvasCtx.fillText(countdown, window.innerWidth / 2, window.innerHeight / 2);
+}
 
 window.animateFrame = (function () {
-    return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            return window.setTimeout(callback, 1000 / 60)
-        }
-    )
-})()
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
 
 function main() {
-    animateFrame(main)
-    draw()
+  if (gameStarted) {
+    draw();
+  } else if (countdown > 0) {
+    drawCountdown();
+  } else {
+    draw();
+  }
+  animateFrame(main);
 }
 
-setup()
-main()
-canvasEl.addEventListener('mousemove', function (e) {
-    mouse.x = e.pageX
-    mouse.y = e.pageY
-})
+function startCountdown() {
+  let countdownInterval = setInterval(() => {
+    countdown--;
+    if (countdown <= 0) {
+      clearInterval(countdownInterval);
+      gameStarted = true;
+    }
+  }, 1000);
+}
+
+function resetGame() {
+  gameStarted = false;
+  countdown = 3;
+  winner = null;
+  score.reset();
+  ball.speed = 4;
+  rightPaddle.speed = 5;
+  ball.x = field.w / 2 + margin;
+  ball.y = field.h / 2 + margin;
+  leftPaddle.y = margin;
+  rightPaddle.y = margin;
+}
+
+setup();
+main();
+
+canvasEl.addEventListener("mousemove", function (e) {
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+});
+
+canvasEl.addEventListener("click", function (e) {
+  const rect = canvasEl.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  if (
+    x > buttons.startButton.x &&
+    x < buttons.startButton.x + buttons.startButton.w &&
+    y > buttons.startButton.y &&
+    y < buttons.startButton.y + buttons.startButton.h
+  ) {
+    buttons.startButton.onClick();
+  }
+
+  if (
+    x > buttons.resetButton.x &&
+    x < buttons.resetButton.x + buttons.resetButton.w &&
+    y > buttons.resetButton.y &&
+    y < buttons.resetButton.y + buttons.resetButton.h
+  ) {
+    buttons.resetButton.onClick();
+  }
+});
